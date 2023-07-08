e399869bd350c89d98e09962e9997a735290f1a1
Xavi Flores <xa_franc@live.com>
2023-07-07 19:00:00 -0500

    class15

--- 02-intro-javascript/src/bases/03-objeto-literal.js
+++ 02-intro-javascript/src/bases/03-objeto-literal.js
@@ -1,17 +1,5 @@
 const persona = {
-    nombre: 'Tony',
-    apellido: 'Stark',
-    edad: 45,
-    direccion: {
-        ciudad: 'New York',
-        zip: 9873652,
-        lat: 145.46584,
-        lng: 215.54645,
-    }
-};
+    nombre: xavier,
+}
 
 console.log(persona);
-
-const persona2 = { ...persona };
-
-console.log(persona2)
