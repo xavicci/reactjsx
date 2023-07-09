69a63b31a5628b7e57cdfcbd4af16a1071176cb7
Xavi Flores <xa_franc@live.com>
2023-07-08 20:16:38 -0500

    chapter23-fetch

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,16 @@
+const apiKey = 'iY5HsCG8gqPRUjPNR0DrRqgVECRlUDpv';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion
+    .then(resp => resp.json())
+    .then(({ data }) => {
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+
+    })
+    .catch(console.warn);
