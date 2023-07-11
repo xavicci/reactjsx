08dc2b6349d92faa9253ce19b1e2abe50093cfee
Xavi Flores <xa_franc@live.com>
2023-07-10 21:57:10 -0500

    chapter37

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,18 @@
 
+const funct_test = (nombre) => nombre;
+
+const newMessage = {
+    message: 'Hola mundito',
+    title: 'Saludo'
+}
 
 export const FirstApp = () => {
     return (
-        <div>
-            Hola soy Xavier Flores C
-        </div>
+        <>
+            {/* <code> {JSON.stringify(newMessage)}</code> */}
+            <h1>{funct_test("BelenXF")}</h1>
+            <h2>Hola! soy Xavier Flores C</h2>
+        </>
     )
 }
 
