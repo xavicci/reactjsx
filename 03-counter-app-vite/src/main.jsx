64c8a906b62db1d6a788707934b5bda7a64ae937
Xavi Flores <xa_franc@live.com>
2023-07-12 07:02:17 -0500

    chapter43

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={12345} />
     </React.StrictMode>
 );
