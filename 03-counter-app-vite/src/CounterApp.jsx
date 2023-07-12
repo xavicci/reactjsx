931945602ac3feff326cf23cd255e1e73837dc82
Xavi Flores <xa_franc@live.com>
2023-07-12 07:16:20 -0500

    chapter44

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+
+    const handleAdd = (event) => {
+        console.log(event)
+    }
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+            <button onClick={handleAdd}>
+                +1
+            </button>
         </>
     )
 };
