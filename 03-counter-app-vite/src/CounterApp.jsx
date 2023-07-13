fc8d297e863eac1c5497bc63144cf6e629d74243
Xavi Flores <xa_franc@live.com>
2023-07-12 19:37:14 -0500

    chapter47

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -9,12 +9,25 @@ export const CounterApp = ({ value }) => {
         setCounter(counter + 1);
     }
 
+    const handleSubtract = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{counter}</h2>
             <button onClick={handleAdd}>
                 +1
+            </button>
+            <button onClick={handleSubtract}>
+                -1
+            </button>
+            <button onClick={handleReset}>
+                Reset
             </button>
         </>
     )
