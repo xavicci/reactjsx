a94919e38e81f6ed1da89cf05cc30be62611accc
Xavi Flores <xa_franc@live.com>
2023-07-30 19:58:38 -0500

    chapter123

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp.jsx';
 // import { CounterApp } from './01-useState/CounterApp.jsx';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx';
-import { SimpleForm } from './02-useEffect/SimpleForm.jsx';
+// import { SimpleForm } from './02-useEffect/SimpleForm.jsx';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <SimpleForm />
+  <FormWithCustomHook />
   // </React.StrictMode>
 )
