ce45d554e6c778b43fd9c56541f25aad2adf4aa2
Xavi Flores <xa_franc@live.com>
2023-08-06 09:47:20 -0500

    chapter158

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,11 +1,26 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { AboutPage } from './AboutPage';
+import { LoginPage } from './LoginPage';
+
 
 export const MainApp = () => {
 
-    
+
     return (
-       <>
-       <h1>MainApp</h1>
-       <hr />
-       </>
+        <>
+            <h1>MainApp</h1>
+            <hr />
+
+            <Routes >
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/login" element={<LoginPage />} />
+
+                <Route path="/*" element={<Navigate to="/about" />} />
+
+            </Routes>
+
+        </>
     )
 }
