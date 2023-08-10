8868d35cf27beeaeb1997c9971f9f91974785d1b
Xavi Flores <xa_franc@live.com>
2023-08-09 22:06:08 -0600

    chapter194

---
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,8 @@
+
+export const HeroPage = () => {
+    return (
+        <div>
+            <h1>HEROPAGE</h1>
+        </div>
+    )
+}
--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,21 +1,13 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-
-import { Navbar } from '../ui';
-
+import { Route, Routes } from 'react-router-dom';
+import { HeroesRoutes } from '../heroes'
 import { LoginPage } from '../auth';
-import { MarvelPage, DcPage } from '../heroes';
-
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
-
             <Routes>
-                <Route path='/marvel' element={<MarvelPage />} />
-                <Route path='/dc' element={<DcPage />} />
                 <Route path='/login' element={<LoginPage />} />
-                <Route path='/*' element={<Navigate to='/marvel' />} />
+                <Route path='/*' element={<HeroesRoutes />} />
             </Routes>
         </>
     )
