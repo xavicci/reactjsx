a9eea361475c9d4b0c799d8bcec23428b245243a
Xavi Flores <xa_franc@live.com>
2023-08-10 22:38:08 -0600

    chapter205

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,13 +1,15 @@
 import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
+import { useMemo } from "react";
 
 export const HeroPage = () => {
 
-    const navigate = useNavigate();
 
     const { id } = useParams();
+    const navigate = useNavigate();
 
-    const hero = getHeroById(id);
+    const hero = useMemo(() => getHeroById(id), [id]);
+
 
     const onReturn = () => {
         const publisher = hero.publisher.split(" ")[0].toLowerCase();
@@ -25,7 +27,7 @@ export const HeroPage = () => {
                 <img
                     src={`/assets/heroes/${id}.jpg`}
                     alt={hero.superhero}
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
             </div>
 
---
+++ 07-heroes-spa/src/hooks/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export const useForm = (initialForm = {}) => {
+
+    const [formState, setFormState] = useState(initialForm);
+
+    // const { username, email, password } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState, [name]: (value)
+        });
+    };
+
+    const onResetForm = () => {
+        setFormState(initialForm);
+    };
+
+    return {
+        ...formState,
+        formState,
+        onResetForm,
+        onInputChange,
+    }
+}
